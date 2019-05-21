c3d36a12eebc255bcf4e3cfd1e0e19e6c0dea48e
Joey Ferenchak <jferench@odd31.cs.ohiou.edu>
2019-05-20 21:48:01 -0400

    Added first homework assignment

---
+++ hw1/git-lab-2-master/git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Joey Ferenchak
+ *        Date: 5/17/19
+ * Description: Prints out Hello Git!
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!" << endl;
+    return 0;
+}// main
